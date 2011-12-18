f43921fff6d1156eebdd722eba96b55ba1ca3c84
Joe Schafer <joe@jschaf.com>
2011-12-19 00:49:08 +0430

    Add GPL2 license.

--- hsearch-mode.el
+++ hsearch-mode.el
@@ -6,6 +6,16 @@
 ;; Version:  0.2
 ;; Keywords:  languages, haskell, hoogle, hayoo
 
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2 of
+;; the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be
+;; useful, but WITHOUT ANY WARRANTY; without even the implied
+;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+;; PURPOSE.  See the GNU General Public License for more details.
+
 ;;; Installation:
 ;;
 ;; Place hsearch-mode.el on your `load-path' by adding this to your
@@ -17,6 +27,8 @@
 ;;
 ;; (autoload 'hsearch "hsearch"
 ;;           "Function to search for Haskell." nil)
+;;
+;; M-x `hsearch' to search for Haskell code
 
 ;;; Commentary:
 ;;
